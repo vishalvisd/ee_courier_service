b3c18d84d2447a922642239a5608fd6bd1e97881
visd <vishaldaga10@gmail.com>
2024-03-30 20:57:26 +0530

    Added bulk user input logic and refactored code

--- src/index.ts
+++ src/index.ts
@@ -4,50 +4,53 @@ import {InquireQuestion, ParsedUserInput} from "./types.js";
 import userInputParser from "./userInputParser.js";
 import totalDeliveryCost, {printResultsToConsole as printDeliveryCostResults} from "./totalDeliveryCost.js";
 import estimatedDeliveryTime, {printResultsToConsole as printDeliveryTimeResults} from "./estimatedDeliveryTime.js";
+import parsePricingDataFile from "./pricingDataFileParser.js";
+
+parsePricingDataFile();
 
 const AVAILABLE_PROGRAMS = {
     TOTAL_DELIVERY_COST: "Total Delivery Cost",
     ESTIMATE_DELIVERY_TIME: "Estimated Delivery Time"
 };
 
-const getProgramSelectionFromUser = ():Promise<string> => {
-    const inputName = "userInput";
-    const inquireInput:InquireQuestion = {
-        type: "list",
-        name: inputName,
-        message: "Select Program:",
-        choices: _.values(AVAILABLE_PROGRAMS)
-    }
-    return inquirer.prompt([inquireInput]).then(v => v[inputName]);
-};
+const AVAILABLE_INPUT_METHODS = {
+    EDITOR: "Editor - provide input in bulk",
+    CMD: "Provide via command line"
+}
 
-const getFromUser = (message: string, inputType: "input" | "editor" = "input"):Promise<string> => {
+type INQUIRE_INPUT_TYPES = "input" | "editor" | "list"
+
+const getFromUser = (message: string, inputType: INQUIRE_INPUT_TYPES = "input", choiceValues?: Array<string>):Promise<string> => {
     const inputName = "userInput";
     const inquireInput: InquireQuestion = {
         type: inputType,
         name: inputName,
-        message: message,
-        choices: _.values(AVAILABLE_PROGRAMS)
+        message: message
     };
-    return inquirer.prompt([inquireInput]).then(v => v[inputName]);
+    if (inputType === "list") inquireInput.choices = choiceValues;
+    return inquirer.prompt([inquireInput]).then(v => _.trim(v[inputName]));
 }
 
-const selectedProgram = await getProgramSelectionFromUser();
+const selectedProgram = await getFromUser("Select Program:", "list", _.values(AVAILABLE_PROGRAMS));
+const selectedInputMethod = await getFromUser("Select Input Method", "list", _.values(AVAILABLE_INPUT_METHODS))
 
 let programInput = '';
+if (selectedInputMethod === AVAILABLE_INPUT_METHODS.EDITOR){
+    programInput = await getFromUser("Please enter to input into default editor", "editor")
+} else {
+    const baseDeliveryCostAndNumberOfPackages = await getFromUser("Base Delivery Cost and Number of Packages [base_delivery_cost no_of_packges]:");
+    programInput += `${baseDeliveryCostAndNumberOfPackages}\n`;
 
-const baseDeliveryCostAndNumberOfPackages = await getFromUser("Base Delivery Cost and Number of Packages [base_delivery_cost no_of_packges]:");
-programInput += `${baseDeliveryCostAndNumberOfPackages}\n`;
+    const numberOfPackages = _.toNumber(_.split(baseDeliveryCostAndNumberOfPackages, /\s+/)[1]);
+    for (let i = 0; i < numberOfPackages; i++){
+        const packageInfo = await getFromUser(`#${i+1}/${numberOfPackages} Package Info [pkg_id1 pkg_weight1_in_kg distance1_in_km offer_code1]:`);
+        programInput += `${packageInfo}\n`;
+    }
 
-const numberOfPackages = _.toNumber(_.split(baseDeliveryCostAndNumberOfPackages, /\s+/)[1]);
-for (let i = 0; i < numberOfPackages; i++){
-    const packageInfo = await getFromUser(`#${i+1}/${numberOfPackages} Package Info [pkg_id1 pkg_weight1_in_kg distance1_in_km offer_code1]:`);
-    programInput += `${packageInfo}\n`;
-}
-
-if (selectedProgram === AVAILABLE_PROGRAMS.ESTIMATE_DELIVERY_TIME) {
-    const fleetInfo = await getFromUser("Fleet Info [no_of_vehicles max_speed max_carriable_weight]:");
-    programInput += `${fleetInfo}\n`;
+    if (selectedProgram === AVAILABLE_PROGRAMS.ESTIMATE_DELIVERY_TIME) {
+        const fleetInfo = await getFromUser("Fleet Info [no_of_vehicles max_speed max_carriable_weight]:");
+        programInput += `${fleetInfo}\n`;
+    }
 }
 
 const parsedInput:ParsedUserInput = userInputParser(programInput);

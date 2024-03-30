aa65aa49b8d65f01151cee4a00d1faf06d5cf6b2
visd <vishaldaga10@gmail.com>
2024-03-30 20:20:28 +0530

    Refactored estimateDeliveryTime logic

--- src/estimatedDeliveryTime.ts
+++ src/estimatedDeliveryTime.ts
@@ -9,45 +9,57 @@ export const printResultsToConsole = (packages: Array<PackageInfo>): void => {
     })
 }
 
-const computeDeliveryTime = (deliveryItems: Array<PackageInfo>, previousLogged: number, fleetSpeed: number):Array<PackageInfo> => {
+const computeDeliveryTimeAndCost = (deliveryItems: Array<PackageInfo>, previousLogged: number, fleetSpeed: number, baseDeliveryCost: number):Array<PackageInfo> => {
     return deliveryItems.map((v:PackageInfo) => {
+        const {deliveryCost, discount} = computeDiscountAndDeliveryCost(v, baseDeliveryCost);
         const wait = _.floor(previousLogged / fleetSpeed, 2) * 2;
         const currentDeliveryTime = _.floor(v.distance/fleetSpeed, 2);
+
         v.deliveryTime = _.round(wait + currentDeliveryTime, 2)
+        v.deliveryCost = deliveryCost;
+        v.discount = discount;
         return v;
     })
 }
 
-export default function (packages: Array<PackageInfo>, fleetInfo: FleetInfo, baseDeliveryCost: number): Array<PackageInfo>{
-    const computeDeliveryTimeCurried = _.curryRight(computeDeliveryTime)(fleetInfo.speed);
-    const copyOfPackages = _.map(packages, (v, i) => Object.assign({}, v, {index: i}));
+const nextAvailableTripPackages = (deliveryPackages: Array<PackageInfo>, maxWeight: number)=>{
+    const selectedPackagesIndex = knapsack(_.map(deliveryPackages, v => v.weight), maxWeight);
+    const nextTripSelectedPackages = _.remove(deliveryPackages, (_v, i) => selectedPackagesIndex.includes(i));
+    return {
+        selectedPackagesIndex,
+        nextTripSelectedPackages,
+        remainingPackages: deliveryPackages
+    }
+}
 
-    // Allocate tasks to delivery vehicles
-    const distnaceLoggedByVehicle = Array(fleetInfo.num).fill(0);
+export default function (packages: Array<PackageInfo>, fleetInfo: FleetInfo, baseDeliveryCost: number): Array<PackageInfo>{
+    const computeDeliveryTimeAndCostCurried = _.curryRight(computeDeliveryTimeAndCost)(fleetInfo.speed, baseDeliveryCost);
+
+    const distanceLoggedByVehicle = Array(fleetInfo.num).fill(0);
     const tasksByVehicle = Array(fleetInfo.num).fill([]);
 
     (function getOrderOfDelivery(deliveryPackages){
         if (_.size(deliveryPackages) > 0){
-            const selectedItems = knapsack(_.map(deliveryPackages, v => v.weight), fleetInfo.maxWeight);
-            const deliveringItems = _.remove(deliveryPackages, (_v, i) => selectedItems.includes(i));
+            // Select Items for next delivery
+            const {selectedPackagesIndex, nextTripSelectedPackages, remainingPackages} = nextAvailableTripPackages(deliveryPackages, fleetInfo.maxWeight)
+            if (selectedPackagesIndex.length <= 0) return;
 
-            //Compute Delivery Time
-            const vehicleChosenIndex = _.indexOf(distnaceLoggedByVehicle, _.min(distnaceLoggedByVehicle));
-            const vehicleDistanceThisTrip = _.max(_.map(deliveringItems, v => v.distance));
-            const previousDistanceLogged = distnaceLoggedByVehicle[vehicleChosenIndex];
-            const deliveringItemsWithDeliveryTime = computeDeliveryTimeCurried(deliveringItems, previousDistanceLogged)
-            _.forEach(deliveringItemsWithDeliveryTime, (v:PackageInfo) => {
-                packages[v.index].deliveryTime = v.deliveryTime;
-                const {deliveryCost, discount} = computeDiscountAndDeliveryCost(v, baseDeliveryCost);
-                packages[v.index].deliveryCost = deliveryCost;
-                packages[v.index].discount = discount;
-            })
-            tasksByVehicle[vehicleChosenIndex].push(deliveringItemsWithDeliveryTime);
-            distnaceLoggedByVehicle[vehicleChosenIndex] =  previousDistanceLogged + vehicleDistanceThisTrip;
+            //Compute Delivery Time and Cost
+            const vehicleChosenIndex = _.indexOf(distanceLoggedByVehicle, _.min(distanceLoggedByVehicle));
+            computeDeliveryTimeAndCostCurried(nextTripSelectedPackages, distanceLoggedByVehicle[vehicleChosenIndex]);
 
-            getOrderOfDelivery(deliveryPackages);
+            // Update Vehicle tasks
+            tasksByVehicle[vehicleChosenIndex].push(nextTripSelectedPackages);
+            distanceLoggedByVehicle[vehicleChosenIndex] += _.max(_.map(nextTripSelectedPackages, v => v.distance));
+
+            // Update Packages with details - delivery time, cost, and discount
+            _.forEach(nextTripSelectedPackages, pkg => {
+                packages[pkg.index] = _.assign({}, packages[pkg.index], pkg)
+            });
+
+            getOrderOfDelivery(remainingPackages);
         }
-    })(copyOfPackages);
+    })(_.map(packages, (v, i) => Object.assign({}, v, {index: i})));
 
     return packages;
 }
--- test/estimatedDeliveryTime.test.ts
+++ test/estimatedDeliveryTime.test.ts
@@ -1,5 +1,7 @@
-import estimatedDeliveryTime from "../src/estimatedDeliveryTime.js";
+import _ from "lodash";
+import estimatedDeliveryTime, {printResultsToConsole} from "../src/estimatedDeliveryTime.js";
 import {FleetInfo, PackageInfo} from "../src/types.js";
+import {printableNumber} from "../src/utils.js";
 
 describe("Delivery time Calculation", ()=>{
     const packages: Array<PackageInfo> = [{
@@ -50,3 +52,41 @@ describe("Delivery time Calculation", ()=>{
         expect(`${packagesWithDetails[4].deliveryTime}`).toBe("4.19");
     })
 });
+
+describe("Logging Results to console are proper", ()=>{
+    const packageOne = {
+        id: "PKG1",
+        weight: 50,
+        distance: 30,
+        couponCode: "OFR001",
+        index: 0
+    };
+    const fleetInfo = {
+        num: 1,
+        speed: 100,
+        maxWeight: 10,
+    }
+    const baseDeliveryCost = 0;
+    it("should log in expected format", ()=>{
+        console.log = jest.fn();
+        const resPackages = estimatedDeliveryTime([packageOne], fleetInfo, baseDeliveryCost);
+        printResultsToConsole(resPackages);
+
+        const resPackage = _.get(resPackages, '[0]');
+        //@ts-ignore
+        expect(console.log.mock.calls[0][0]).toBe(`${resPackage.id} ${printableNumber(`${resPackage.discount}`)} ${printableNumber(`${resPackage.deliveryCost}`)} ${printableNumber(`${resPackage.deliveryTime}`)}`);
+    });
+
+    it("should display N/A for values which are not available", ()=>{
+        console.log = jest.fn();
+        const resPackages = estimatedDeliveryTime([packageOne], fleetInfo, baseDeliveryCost);
+
+        printResultsToConsole(resPackages);
+        //@ts-ignore
+        expect(console.log.mock.calls[0][0]).toBe(`${packageOne.id} N/A N/A N/A`);
+    });
+
+    afterEach(()=>{
+        jest.resetAllMocks();
+    })
+})
